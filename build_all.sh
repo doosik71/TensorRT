c353111cb6c38bbc2e0d5a5f42ef1768bead3cc0
Doosik Kim <doosik71@gmail.com>
2020-03-18 01:00:48 +0900

    Add comment.

--- build_all.sh
+++ build_all.sh
@@ -1,3 +1,7 @@
+# git clone -b release/6.0 git@github.com:doosik71/TensorRT.git
+# cd TensorRT
+# git submodule update --init --recursive
+
 mkdir ./build
 cd ./build
 export TRT_RELEASE=`pwd`/TensorRT-7.0.0.11
